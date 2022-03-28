d7dbfbf181eb9cd1e442374cb0da3150e3a2a198
aetokmakov <tokmakov.anton@yandex.ru>
2022-03-28 17:38:09 +0300

    Initial commit

--- src/main/kotlin/com/aetokmakov/sku/Main.kt
+++ src/main/kotlin/com/aetokmakov/sku/Main.kt
@@ -1,14 +1,1 @@
-import com.aetokmakov.sku.model.ProductMapping
-import com.aetokmakov.sku.service.ProductAggregatorService
-
-fun main(args: Array<String>) {
-    val products = listOf("CVCD", "SDFD", "DDDF", "SDFD")
-    val mappings = mapOf(
-        "CVCD" to ProductMapping(1, 'X'),
-        "SDFD" to ProductMapping(2, 'Z'),
-        "DDDF" to ProductMapping(1)
-    )
-
-    val productAggregatorService = ProductAggregatorService()
-    print(productAggregatorService.aggregate(products, mappings))
-}
+fun main(args: Array<String>) {}
